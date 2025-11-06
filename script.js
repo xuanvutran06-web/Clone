5ca5044d360f0b1062e61c9b997dfc9d104dd387
xuanvutran06-web <xuanvu.tran06@gmail.com>
2025-11-06 15:29:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,4 @@ document.querySelectorAll('.price').forEach(el => {
   el.classList.remove('show');
 });
 
-document.querySelectorAll('input[type="checkbox"][data-target]').forEach(cb => {
-  cb.addEventListener('change', function () {
-    const id = this.getAttribute('data-target');
-    const el = document.getElementById(id);
-    const value = el.getAttribute('data-price');
-
-    if (this.checked) {
-      el.textContent = value;
-      el.classList.add('show');   // thêm class để fade-in
-    } else {
-      el.textContent = '';
-      el.classList.remove('show');
-    }
-  });
-});
+document.querySelectorAll('input
